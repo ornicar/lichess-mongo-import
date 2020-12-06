0f03bec68232d337e04816026ad2e3ddb687ce30
Thibault Duplessis <t@lichess.org>
2020-12-06 13:14:57 +0100

    copy games by chunks of 1000

--- puzzle-game.ts
+++ puzzle-game.ts
@@ -2,7 +2,7 @@ import config from './config';
 import { Dbs, run, copyManyIds, drainBatch } from './importer';
 
 async function all(dbs: Dbs) {
-  await drainBatch('puzzle', dbs.puzzler.collection('puzzle2_puzzle').find(), 512, async ps => {
+  await drainBatch('puzzle', dbs.puzzler.collection('puzzle2_puzzle').find(), 1000, async ps => {
     await copyManyIds(dbs, config.coll.game, ps.map(p => p.gameId));
   });
 }
