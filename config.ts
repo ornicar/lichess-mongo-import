e9ce65c0f591ef82ad589f2916dccad931ad8439
Thibault Duplessis <t@lichess.org>
2023-01-24 12:38:05 +0100

    0.0.0.0 connects better than localhost

--- config.ts
+++ config.ts
@@ -1,10 +1,9 @@
 const replicargs = 'directConnection=true&serverSelectionTimeoutMS=2000&appName=importer';
-const args = 'directConnection=true&serverSelectionTimeoutMS=2000&appName=importer';
 export default {
   source: `mongodb://localhost:27117/lichess?readPreference=secondary&${replicargs}`,
   dest: `mongodb://0.0.0.0:27017/lichess`,
-  puzzler: `mongodb://localhost:27017/puzzler?${args}`,
-  study: `mongodb://localhost:27118/study?${replicargs}`,
+  puzzler: `mongodb://0.0.0.0:27017/puzzler`,
+  study: `mongodb://0.0.0.0:27118/study?${replicargs}`,
   dbName: `lichess`,
   coll: {
     user: `user4`,
