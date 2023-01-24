aafe6dd0e92858f2915ce8b2a532794b5dcc2692
Thibault Duplessis <t@lichess.org>
2023-01-24 12:38:10 +0100

    update puzzle-game

--- puzzle-game.ts
+++ puzzle-game.ts
@@ -5,14 +5,21 @@ async function all(dbs: Dbs) {
   const puzzlerDb = await dbs.puzzler();
   const source = await dbs.source();
   const dest = await dbs.dest();
-  await drainBatch('puzzle', puzzlerDb.collection('puzzle2_puzzle').find(), 1000, async ps => {
-    await copyManyIds(
-      source,
-      dest,
-      config.coll.game,
-      ps.map(p => p.gameId)
-    );
-  });
+  await drainBatch(
+    'puzzle',
+    puzzlerDb
+      .collection('puzzle2_puzzle')
+      .find({ createdAt: { $gt: new Date(Date.now() - 1000 * 3600 * 24 * 7 * 2) } }),
+    1000,
+    async ps => {
+      await copyManyIds(
+        source,
+        dest,
+        config.coll.game,
+        ps.map(p => p.gameId)
+      );
+    }
+  );
 }
 
 run((dbs, _) => all(dbs));
