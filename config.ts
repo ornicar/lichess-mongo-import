a9a05e4cc55c63e7a4743828f70c3d986f038a7f
Thibault Duplessis <t@lichess.org>
2024-10-09 10:33:01 +0200

    fix puzzler db config, fixes puzzle generation on crom

--- config.ts
+++ config.ts
@@ -3,7 +3,7 @@ const replicargs =
 export default {
   source: `mongodb://localhost:27117/lichess?readPreference=secondary&${replicargs}`,
   dest: `mongodb://0.0.0.0:27017/lichess`,
-  puzzler: `mongodb://0.0.0.0:27017/lichess`,
+  puzzler: `mongodb://0.0.0.0:27017/puzzler`,
   study: `mongodb://0.0.0.0:27118/study?${replicargs}`,
   dbName: `lichess`,
   coll: {
