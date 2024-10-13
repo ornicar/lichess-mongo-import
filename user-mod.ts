e181c6ccc4a93685fa26269b2a843fc592f012fc
Thibault Duplessis <t@lichess.org>
2024-10-13 10:18:05 +0200

    fix user-mod

--- user-mod.ts
+++ user-mod.ts
@@ -1,21 +1,38 @@
-import config from './config';
-import { Dbs, run, copyManyIds, drainBatch, copySelect } from './importer';
+import config from "./config";
+import {
+  Dbs,
+  run,
+  copyManyIds,
+  drainBatch,
+  copySelect,
+  insert,
+} from "./importer";
 
 async function one(dbs: Dbs, id: any) {
   const main = await dbs.source();
   const dest = await dbs.dest();
   id = id.toLowerCase();
-  const user = await main.db().collection(config.coll.user).findOne({ _id: id });
-  if (!user) throw 'User not found';
-  await dest.db().collection(config.coll.user).deleteOne({ _id: user.id });
-  await dest.db().collection(config.coll.user).insertOne(user);
-  await copySelect(main.db(), dest.db(), config.coll.report, { user: user.id });
-  await copySelect(main.db(), dest.db(), config.coll.report, { 'atoms.by': user.id });
-  await copySelect(main.db(), dest.db(), config.coll.appeal, { '_id': user.id });
-  await copySelect(main.db(), dest.db(), config.coll.note, { to: user.id });
-  await copySelect(main.db(), dest.db(), config.coll.modlog, { user: user.id });
-  await copySelect(main.db(), dest.db(), config.coll.playban, { _id: user.id });
-  await copySelect(main.db(), dest.db(), config.coll.shutup, { _id: user.id });
+  const user = await main
+    .db()
+    .collection(config.coll.user)
+    .findOne({ _id: id });
+  if (!user) throw "User not found";
+  insert(dest.db().collection(config.coll.user), user);
+  await copySelect(main.db(), dest.db(), config.coll.report, {
+    user: user._id,
+  });
+  await copySelect(main.db(), dest.db(), config.coll.report, {
+    "atoms.by": user._id,
+  });
+  await copySelect(main.db(), dest.db(), config.coll.appeal, { _id: user._id });
+  await copySelect(main.db(), dest.db(), config.coll.note, { to: user._id });
+  await copySelect(main.db(), dest.db(), config.coll.modlog, {
+    user: user._id,
+  });
+  await copySelect(main.db(), dest.db(), config.coll.playban, {
+    _id: user._id,
+  });
+  await copySelect(main.db(), dest.db(), config.coll.shutup, { _id: user._id });
 }
 
 run((dbs, args) => one(dbs, args[0]));
