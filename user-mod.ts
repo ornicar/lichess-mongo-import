b0f9dc3f3f877c8393b627a76b4ef12de1396077
Thibault Duplessis <t@lichess.org>
2024-11-04 17:10:31 +0100

    tweaks

--- user-mod.ts
+++ user-mod.ts
@@ -33,19 +33,29 @@ async function one(dbs: Dbs, id: any) {
     _id: user._id,
   });
   await copySelect(main.db(), dest.db(), config.coll.shutup, { _id: user._id });
-  const reports = await dest.db().collection(config.coll.report).find({ user: user._id }).toArray();
-  await copySelect(main.db(), dest.db(), config.coll.user, { _id: { $in: reports.flatMap(r => r.atoms.map((a: any) => a.by)) } })
-
-  await drainBatch(config.coll.msgThread, main
+  const reports = await dest
     .db()
-    .collection(config.coll.msgThread)
-    .find({ users: id }), 20, async (threads) => {
+    .collection(config.coll.report)
+    .find({ user: user._id })
+    .toArray();
+  await copySelect(main.db(), dest.db(), config.coll.user, {
+    _id: { $in: reports.flatMap((r) => r.atoms.map((a: any) => a.by)) },
+  });
+
+  await drainBatch(
+    config.coll.msgThread,
+    main.db().collection(config.coll.msgThread).find({ users: id }),
+    20,
+    async (threads) => {
       await dest
         .db()
-        .collection(config.coll.relayTour)
+        .collection(config.coll.msgThread)
         .insertMany(threads, { ordered: false });
-      await copySelect(main.db(), dest.db(), config.coll.msgMsg, { tid: { $in: threads.map(t => t._id) } });
-    });
+      await copySelect(main.db(), dest.db(), config.coll.msgMsg, {
+        tid: { $in: threads.map((t) => t._id) },
+      });
+    },
+  );
 }
 
 run((dbs, args) => one(dbs, args[0]));
